2b5e18481ecef5d5ba39e9445758eb9b944209b1
Leonid2377 <leonid2377@mail.ru>
2022-10-16 12:42:39 +0300

    version 1.2

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,8 +1,12 @@
+import csv
+from collections import defaultdict
 from datetime import datetime as dt
 import os
 
 import sqlalchemy as db
 from sqlalchemy.orm import declarative_base, Session
+
+TIME_FORMAT = '%Y-%m-%dT%H-%M-%S'
 
 Base = declarative_base()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -17,6 +21,9 @@ class Pep(Base):
 
 
 class PepParsePipeline:
+    status_count = defaultdict(int)
+    total = 0
+
     def open_spider(self, spider):
         engine = db.create_engine('sqlite:///posts.db')
         Base.metadata.create_all(engine)
@@ -31,24 +38,29 @@ class PepParsePipeline:
         if not self.session.query(Pep).filter(Pep.number == pep.number).all():
             self.session.add(pep)
             self.session.commit()
+        if item.get('status'):
+            self.total += 1
+            self.status_count[
+                item['status']] = self.status_count.get(
+                item['status'], 0) + 1
         return item
 
+
     def close_spider(self, spider):
+        now_time = dt.now().strftime(TIME_FORMAT)
         filename = os.path.join(
             BASE_DIR,
             'results',
-            f'status_summary_{dt.now().strftime("%Y-%m-%dT%H-%M-%S")}.csv'
+            f'status_summary_{now_time}.csv'
         )
+        results = [('Cтатус', 'Количество')]
+        results.extend(self.status_count.items())
+        results.append(('Total: ', self.total))
         with open(filename, mode='w', encoding='utf-8') as f:
-            f.write('Статус,Количество\n')
-            total = 0
-            for status, status_count in self.session.execute(
-                db.select([
-                    Pep.status,
-                    db.func.count(Pep.status).label('status_count')
-                ]).group_by(Pep.status)
-            ):
-                f.write(f'{status},{status_count}\n')
-                total += status_count
-            f.write(f'Total,{total}\n')
+            db.select([
+                Pep.status,
+                db.func.count(Pep.status).label('status_count')
+            ]).group_by(Pep.status)
+            writer = csv.writer(f)
+            writer.writerows(results)
         self.session.close()
